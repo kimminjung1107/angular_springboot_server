a9d891f6fb8737cdf87dae944e77789493a33fe6
kmjminjung1107 <hmh5225@naver.com>
2018-05-25 14:49:41 +0900

    url 주소 변경

--- src/main/java/myspringboot/hero/HeroController.java
+++ src/main/java/myspringboot/hero/HeroController.java
@@ -6,15 +6,16 @@ import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping(value = "/heros")
+@RequestMapping(value = "/heroes")
 public class HeroController {
 	
 	private List<Hero> heros = new ArrayList<>();	
